58bfe4abb8471d455bff0acebfe088136479430d
ThywillJoshua <76951731+ThywillJoshua@users.noreply.github.com>
2021-11-23 11:57:14 +0100

    geolocation

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//Geo Location
+//If Geo Location
+if (navigator.geolocation) {
+  //Set Geolocation
+  navigator.geolocation.getCurrentPosition(
+    //Success Func
+    function (position) {
+      //Get latitude and longitude
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      console.log(latitude, longitude);
+
+      //Test on Google
+      console.log(
+        `https://www.google.com/maps/@${41.88167760439392},${12.572400570502998}`
+      );
+    },
+
+    //Error Func
+    function () {
+      alert('Could not get your position');
+    }
+  );
+}
