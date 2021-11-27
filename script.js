60c3438ac9adb4b3c609a1f4d5d484cab733c8a4
ThywillJoshua <76951731+ThywillJoshua@users.noreply.github.com>
2021-11-27 14:56:28 +0100

    zoom out on submit

--- script.js
+++ script.js
@@ -335,7 +335,7 @@ class App {
     let viewport = document.querySelector('meta[name="viewport"]');
 
     if (viewport) {
-      viewport.content = 'initial-scale=0.1';
+      viewport.content = 'initial-scale=1';
       // viewport.content = 'width=1200';
     }
   }
